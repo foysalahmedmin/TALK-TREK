e0df678aed18824150af12f458a61bf4d01655c3
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-06-13 18:54:41 +0600

    feedback modify

--- src/pages/Dashboard/Admin/ManageClasses/FeedBack.jsx
+++ src/pages/Dashboard/Admin/ManageClasses/FeedBack.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 
 const FeedBack = ({ feedbackClass }) => {
     const [axiosSecure] = useAxiosSecure()
     const { _id, className, instructorName, instructorEmail } = feedbackClass;
-    const feedbackHandler = (e) => {
-        e.preventDefault()
-        const message = e.target.feedback.value;
-        if(message){
-            axiosSecure.put(`/admin/feedback`, {feedback: message})
-            .then(result => {
-                console.log(result.data)
-            })
+    const [message, setMessage] = useState('')
+    const messageHandler = (e) => setMessage(e.target.value)
+    const feedbackHandler = () => {
+        if (message) {
+            axiosSecure.put(`/admin/feedback/${_id}`, { feedback: message })
+                .then(result => {
+                    console.log(result.data)
+                })
         }
     }
     return (
@@ -19,14 +19,16 @@ const FeedBack = ({ feedbackClass }) => {
             <dialog id="my_modal_3" className="modal">
                 <form method="dialog" className="modal-box text-center">
                     <button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                    <form onSubmit={feedbackHandler}>
+                    <div>
                         <h3 className="font-bold text-center uppercase text-xl">{className}</h3>
                         <p className="text-center uppercase font-light tracking-[3px]">Instructor: {instructorName}</p>
-                        <p className='text-center'>{instructorEmail}</p>
-                        <label htmlFor="feedback"></label>
-                        <textarea placeholder="Feedback" name='feedback' id='feedback' className="textarea textarea-bordered textarea-md w-full" ></textarea>
-                        <input className='primary-btn w-full' type="submit" value="FEEDBACK" />
-                    </form>
+                        <p className='text-center mb-5'>{instructorEmail}</p>
+                        <div>
+                            <label htmlFor="feedback"></label>
+                            <textarea onChange={messageHandler} placeholder="Feedback" name='feedback' id='feedback' className="textarea textarea-bordered textarea-md w-full mb-3" ></textarea>
+                            <button onClick={feedbackHandler} className='primary-btn w-full' >FEEDBACK</button>
+                        </div>
+                    </div>
                 </form>
             </dialog>
         </>
