49d47a9d9a0962611168ff4bb9b204082331c37e
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-06-14 23:43:12 +0600

    footer

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,16 +1,16 @@
-import { HiChevronRight, HiOutlineLocationMarker, HiLocationMarker, HiOutlineTag, HiOutlineClock } from "react-icons/hi";
+import { HiPhone, HiOutlineLocationMarker, HiShare } from "react-icons/hi";
 import logo from "../../../assets/TalkTrekLogo.svg"
 import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer className='bg-base-300 py-10'>
+        <footer className='bg-base-300 py-5'>
             <div className="container">
                 <div className='grid grid-cols-4 py-3 justify-between gap-5'>
                     <div className="flex h-full items-center">
                     <Link to='/' className='mr-auto'>
                         <div className='flex gap-1 items-center'>
-                            <img className="w-10" src={logo} alt="" />
+                            <img className="w-20" src={logo} alt="" />
                             <p>
                                 <span className="font-semibold text-secondary">Talk</span><span className="font-bold text-xl text-primary">Trek</span>
                             </p>
@@ -26,19 +26,19 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className='flex'>
-                        <div><HiOutlineTag className='text-4xl mr-3 text-primary' /></div>
+                        <div><HiPhone className='text-4xl mr-3 text-primary' /></div>
                         <div>
-                            <h3 className='text-2xl mb-5 font-semibold'>Reservations</h3>
+                            <h3 className='text-2xl mb-5 font-semibold'>Contact</h3>
                             <p className='opacity-50'>+94 423-23-221</p>
-                            <p className='opacity-50'>bengaldining@chef.com</p>
+                            <p className='opacity-50'>talktrekg@language.com</p>
                         </div>
                     </div>
                     <div className='flex'>
-                        <div><HiOutlineClock className='text-4xl mr-3 text-primary' /></div>
+                        <div><HiShare className='text-4xl mr-3 text-primary' /></div>
                         <div>
-                            <h3 className='text-2xl mb-5 font-semibold'>Opening Hours</h3>
-                            <p className='opacity-50'>Mon - Sat: 10am - 11pm</p>
-                            <p className='opacity-50'>Sunday: Closed</p>
+                            <h3 className='text-2xl mb-5 font-semibold'>Service</h3>
+                            <p className='opacity-50'>Formal Language</p>
+                            <p className='opacity-50'>International Connection</p>
                         </div>
                     </div>
                 </div>
